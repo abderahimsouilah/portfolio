452c0cd6ef3ec6d43f1c8baa2a67fd720e5e9398
Souilah abderahim <120774252+abderahimsouilah@users.noreply.github.com>
2023-12-24 13:28:50 +0100

    nice

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    mongo:
-      'mongodb+srv://souilah:souilah@cluster0.u4dgsxh.mongodb.net/contact?retryWrites=true&w=majority',
-  },
 };
 
 module.exports = nextConfig;
